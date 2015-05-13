74e7c5f6dc46cd15e79165bacd0f1debbc38ad2a
Llorenc Marti <llorens.marti@gmail.com>
2015-05-13 00:18:10 -0700

    test-stronghold now is easy to setup

--- s/test-stronghold.py
+++ s/test-stronghold.py
@@ -11,24 +11,34 @@ def get_red_string(string):
 def get_green_string(string):
     return '\033[92m' + string + '\033[0m'
 
+def get_packed_app_info(tags, path):
+    obj = {}
+    obj['tags'] = tags
+    obj['path'] = path
+    return obj
+
 def get_test_list():
     """ Returns a list of tests, every test its a list of commands """
+
+    packs = []
+    packs.append(get_packed_app_info(['app', 'gitrepo'],            'app/service/gitrepo',))
+    packs.append(get_packed_app_info(['app', 'gitfeeder'],          'app/service/gitfeeder',))
+    packs.append(get_packed_app_info(['app', 'strongholdworker'],   'app/service/strongholdworker',))
+    packs.append(get_packed_app_info(['app', 'httpcommon'],         'app/util/httpcommon',))
+    packs.append(get_packed_app_info(['app', 'commandrepo'],        'app/util/commandrepo',))
+    packs.append(get_packed_app_info(['app', 'bluesteel'],          'app/service/bluesteel',))
+
     test_list = []
     test_list.append((['pip'], ['python', 's/internal/test-pip-installed.py']))
     test_list.append((['requirements'], ['python', 's/internal/test-pip-requirements.py']))
     test_list.append((['database'], ['python', 's/internal/test-db.py']))
-    test_list.append((['app', 'gitrepo'], ['python', 'manage.py', 'test', 'app/service/gitrepo/tests/', '--settings=stronghold.settings.testing']))
-    test_list.append((['app', 'gitfeeder'], ['python', 'manage.py', 'test', 'app/service/gitfeeder/tests/', '--settings=stronghold.settings.testing']))
-    test_list.append((['app', 'strongholdworker'], ['python', 'manage.py', 'test', 'app/service/strongholdworker/tests/', '--settings=stronghold.settings.testing']))
-    test_list.append((['app', 'httpcommon'], ['python', 'manage.py', 'test', 'app/util/httpcommon/tests/', '--settings=stronghold.settings.testing']))
-    test_list.append((['app', 'commandrepo'], ['python', 'manage.py', 'test', 'app/util/commandrepo/tests/', '--settings=stronghold.settings.testing']))
-    test_list.append((['app', 'bluesteel'], ['python', 'manage.py', 'test', 'app/service/bluesteel/tests/', '--settings=stronghold.settings.testing']))
-    test_list.append((['pylint'], ['pylint', '--rcfile=s/internal/pylint-config-file.py', '--generated-members=objects', '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}', 'app/service/gitrepo']))
-    test_list.append((['pylint'], ['pylint', '--rcfile=s/internal/pylint-config-file.py', '--generated-members=objects', '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}', 'app/service/gitfeeder']))
-    test_list.append((['pylint'], ['pylint', '--rcfile=s/internal/pylint-config-file.py', '--generated-members=objects', '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}', 'app/service/strongholdworker']))
-    test_list.append((['pylint'], ['pylint', '--rcfile=s/internal/pylint-config-file.py', '--generated-members=objects', '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}', 'app/util/httpcommon']))
-    test_list.append((['pylint'], ['pylint', '--rcfile=s/internal/pylint-config-file.py', '--generated-members=objects', '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}', 'app/util/commandrepo']))
-    test_list.append((['pylint'], ['pylint', '--rcfile=s/internal/pylint-config-file.py', '--generated-members=objects', '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}', 'app/service/bluesteel']))
+
+    for pack in packs:
+        test_list.append((pack['tags'], ['python', 'manage.py', 'test', '{0}/tests/'.format(pack['path']), '--settings=stronghold.settings.testing']))
+
+    for pack in packs:
+        test_list.append((['pylint'], ['pylint', '--rcfile=s/internal/pylint-config-file.py', '--generated-members=objects', '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}', pack['path']]))
+
     return test_list
 
 def main(args=sys.argv):
@@ -39,7 +49,9 @@ def main(args=sys.argv):
 
     work_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 
-    for test in get_test_list():
+    test_list = get_test_list()
+
+    for test in test_list:
         if len(tag_filter) > 0 and (tag_filter not in test[0]):
             continue
 
