0caed7d91143f724fc97c0b7bc5dba9ac2e943cb
Llorenc Marti <llorens.marti@gmail.com>
2015-05-23 12:55:47 -0700

    Added BluesteelManager

--- app/presenter/views/ViewMain.py
+++ app/presenter/views/ViewMain.py
@@ -2,9 +2,13 @@
 
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+# from app.service.bluesteel.models.BluesteelLayourModel import BluesteelLayourEntry
+
 
 def get_main(request):
     """ Returns html for the main page """
+    # layout_entries = BluesteelLayourEntry.objects.all()
+
     template = loader.get_template('presenter/main.html')
     context = RequestContext(
         request,
--- app/service/bluesteel/models/BluesteelProjectModel.py
+++ app/service/bluesteel/models/BluesteelProjectModel.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class BluesteelProjectEntry(models.Model):
     """ BlueSteel Project """
+    layout = models.ForeignKey('bluesteel.BluesteelLayoutEntry', related_name='bluesteel_layout')
     archive = models.CharField(default='', max_length=50)
     name = models.CharField(default='', max_length=50)
     git_project = models.ForeignKey('gitrepo.GitProjectEntry', related_name='bluesteel_git_project')
@@ -11,7 +12,8 @@ class BluesteelProjectEntry(models.Model):
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
     def __unicode__(self):
-        return u'Bluesteel Project archive:{0}, name:{1}, git_project{2}'.format(
+        return u'Bluesteel Project layout:{0}, archive:{1}, name:{2}, git_project{3}'.format(
+            self.layout.name,
             self.archive,
             self.name,
             self.git_project.id
