457b6527befa47c6d7657c85d21be6d89082c0cd
Llorenc Marti <llorens.marti@gmail.com>
2015-10-31 10:13:00 -0700

    Benchmark models registered

--- app/service/benchmark/admin.py
+++ app/service/benchmark/admin.py
@@ -1,5 +1,8 @@
 """ Benchmark Admin """
 
-# from django.contrib import admin
+from django.contrib import admin
+from app.service.benchmark.models.BenchmarkDefinitionModel import BenchmarkDefinitionEntry
+from app.service.benchmark.models.BenchmarkExecutionModel import BenchmarkExecutionEntry
 
-# Register your models here.
+admin.site.register(BenchmarkDefinitionEntry)
+admin.site.register(BenchmarkExecutionEntry)
