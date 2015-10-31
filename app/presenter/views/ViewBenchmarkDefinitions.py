0f5a4edf0f5d8322c0b9de798b7634f4f30fb72d
Llorenc Marti <llorens.marti@gmail.com>
2015-10-31 11:47:37 -0700

    Added Add Definition to Definitions page

--- app/presenter/views/ViewBenchmarkDefinitions.py
+++ app/presenter/views/ViewBenchmarkDefinitions.py
@@ -8,15 +8,15 @@ from app.util.httpcommon import res
 def get_benchmark_definitions(request):
     """ Returns html for the benchmark definition page """
 
-    # control = {}
-    # control['name'] = '  Add Definition'
-    # control['link'] = ViewUrlGenerator.get_layout_create_url()
-    # control['icon'] = 'fa fa-plus'
+    control = {}
+    control['name'] = '  Add Definition'
+    control['link'] = ''
+    control['icon'] = 'fa fa-plus'
     # control['onclick'] = 'create_new_layout(this, \'{0}\');'.format(control['link'])
 
     data = {}
     data['menu'] = ViewPrepareObjects.prepare_menu_for_html([])
-    # data['controls'] = []
-    # data['controls'].append(control)
+    data['controls'] = []
+    data['controls'].append(control)
 
     return res.get_template_data(request, 'presenter/benchmark_definitions.html', data)
