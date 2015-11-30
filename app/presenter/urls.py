5539b0f5302fd9ba2427cf32e0c06b0db2b2495f
Llorenc Marti <llorencmarti@MacBook-Air-de-Llorenc.local>
2015-11-30 21:53:43 +0900

    Renamed some views to html views

--- app/presenter/urls.py
+++ app/presenter/urls.py
@@ -6,19 +6,19 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^view/$',
-        'app.presenter.views.ViewMain.get_main'),
+        'app.presenter.views.html.ViewHtmlMain.get_main'),
 
     url(r'^layout/all/$',
-        'app.presenter.views.ViewLayout.get_layouts'),
+        'app.presenter.views.html.ViewHtmlLayout.get_layouts'),
 
     url(r'^layout/edit/(?P<layout_id>\d+)/$',
-        'app.presenter.views.ViewLayout.get_layout_editable'),
+        'app.presenter.views.html.ViewHtmlLayout.get_layout_editable'),
 
     url(r'^layout/(?P<layout_id>\d+)/add/project/$',
-        'app.presenter.views.ViewLayout.add_default_project'),
+        'app.presenter.views.html.ViewHtmlLayout.add_default_project'),
 
     url(r'^layout/(?P<layout_id>\d+)/confirm/delete/$',
-        'app.presenter.views.ViewLayout.confirm_delete'),
+        'app.presenter.views.html.ViewHtmlLayout.confirm_delete'),
 
     url(r'^layout/(?P<layout_id>\d+)/save/$',
         'app.presenter.views.json.ViewJsonLayout.save_layout'),
@@ -30,7 +30,7 @@ urlpatterns = patterns(
         'app.presenter.views.json.ViewJsonLayout.post_create_new_layout'),
 
     url(r'^projects/$',
-        'app.presenter.views.ViewProject.get_projects'),
+        'app.presenter.views.html.ViewHtmlProject.get_projects'),
 
     url(r'^project/(?P<project_id>\d+)/save/$',
         'app.presenter.views.json.ViewJsonProject.save_project'),
@@ -39,22 +39,22 @@ urlpatterns = patterns(
         'app.presenter.views.json.ViewJsonProject.delete_project'),
 
     url(r'^project/(?P<project_id>\d+)/branch/all/$',
-        'app.presenter.views.ViewProject.get_project_branches'),
+        'app.presenter.views.html.ViewHtmlProject.get_project_branches'),
 
     url(r'^project/(?P<project_id>\d+)/branch/(?P<branch_id>\d+)/$',
-        'app.presenter.views.ViewProject.get_project_single_branch'),
+        'app.presenter.views.html.ViewHtmlProject.get_project_single_branch'),
 
     url(r'^workers/$',
-        'app.presenter.views.ViewWorkers.get_workers'),
+        'app.presenter.views.html.ViewHtmlWorkers.get_workers'),
 
     url(r'^worker/(?P<worker_id>\d+)/report/all/$',
-        'app.presenter.views.ViewWorkers.get_worker_reports'),
+        'app.presenter.views.html.ViewHtmlWorkers.get_worker_reports'),
 
     url(r'^definitions/all/$',
-        'app.presenter.views.ViewBenchmarkDefinitions.get_benchmark_definitions'),
+        'app.presenter.views.html.ViewHtmlBenchmarkDefinitions.get_benchmark_definitions'),
 
     url(r'^definition/(?P<definition_id>\d+)/edit/$',
-        'app.presenter.views.ViewBenchmarkDefinitions.get_benchmark_definition_edit'),
+        'app.presenter.views.html.ViewHtmlBenchmarkDefinitions.get_benchmark_definition_edit'),
 
     url(r'^definitions/create/$',
         'app.presenter.views.json.ViewJsonBenchmarkDefinitions.create_new_benchmark_definition'),
