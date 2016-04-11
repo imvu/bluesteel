39dac38280fd4ef7dabda6c637e4c40d0fc5ce0d
Llorenc Marti <llorens.marti@gmail.com>
2016-04-10 17:57:54 -0700

    Added a function to increase the size of Sqlite cache. I don't have yet a better place to put that function.

--- app/logic/bluesteel/__init__.py
+++ app/logic/bluesteel/__init__.py
@@ -1,1 +1,15 @@
 """ Automatic file """
+
+#Find a better place for this code that will set the cache of Sqlite
+
+from django.db.backends.signals import connection_created
+
+def activate_foreign_keys(sender, connection, **kwargs):
+    """Enable integrity constraint with sqlite."""
+    del sender
+    del kwargs
+    if connection.vendor == 'sqlite':
+        cursor = connection.cursor()
+        cursor.execute('PRAGMA main.cache_size = -1000000;')
+
+connection_created.connect(activate_foreign_keys)
