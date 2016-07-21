937691068eabc3d32afb522c9f4a5bb40dbf6190
Llorenç Marti Garcia <llorens.marti@gmail.com>
2016-07-20 22:01:48 -0700

    updated settings to reflect readme

--- bluesteel/settings/testing.py
+++ bluesteel/settings/testing.py
@@ -88,10 +88,10 @@ DATABASES = {
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'rowpowdb', # Or path to database file if using sqlite3.
+#         'NAME': 'bluesteeldb',
 #         # The following settings are not used with sqlite3:
-#         'USER': 'rowpowuser',
-#         'PASSWORD': 'sandcastle',
+#         'USER': 'bluesteeluser',
+#         'PASSWORD': 'pass',
 #         'HOST': 'localhost',    # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
 #         'PORT': '', # Set to empty string for default.
 #     }
