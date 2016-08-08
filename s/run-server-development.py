704e12cada8441d6e16ad4b4cc0e410c60611161
Llorenç Marti Garcia <llorens.marti@gmail.com>
2016-08-07 17:30:36 -0700

    Now before running the server, we check if all the pip requirements are installed

--- s/run-server-development.py
+++ s/run-server-development.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 def main():
+    subprocess.call(['python', 's/internal/install-pip-requirements.py'])
     subprocess.call(['python', 'manage.py', 'migrate', '--settings=bluesteel.settings.development'])
     subprocess.call(['python', 'manage.py', 'runserver', '28028', '--settings=bluesteel.settings.development'])
 
--- s/run-server-production.py
+++ s/run-server-production.py
@@ -17,6 +17,7 @@ def main():
         shutil.rmtree(static_dir)
     os.makedirs(static_dir)
 
+    subprocess.call(['python', 's/internal/install-pip-requirements.py'])
     subprocess.call(['python', 'manage.py', 'migrate', '--settings=bluesteel.settings.production'])
     subprocess.call(['python', 'manage.py', 'collectstatic', '--settings=bluesteel.settings.production', '--noinput'])
     subprocess.call(['python', 'manage.py', 'runserver', '0.0.0.0:8080', '--settings=bluesteel.settings.production'])
