97a4fa1dfaeabecdd6f57560d221fc6c5c51e7c5
Llorenç Marti Garcia <llorens.marti@gmail.com>
2016-09-11 17:37:57 -0700

    Added some values to grid css and modified the template file to show it, this last edition needs to be replaced with the propper logic

--- app/logic/benchmark/admin.py
+++ app/logic/benchmark/admin.py
@@ -3,6 +3,8 @@
 from django.contrib import admin
 from app.logic.benchmark.models.BenchmarkDefinitionModel import BenchmarkDefinitionEntry
 from app.logic.benchmark.models.BenchmarkExecutionModel import BenchmarkExecutionEntry
+from app.logic.benchmark.models.BenchmarkFluctuationOverrideModel import BenchmarkFluctuationOverrideEntry
 
 admin.site.register(BenchmarkDefinitionEntry)
 admin.site.register(BenchmarkExecutionEntry)
+admin.site.register(BenchmarkFluctuationOverrideEntry)
