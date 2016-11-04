30ff5320a50b222d0417ee7aa5bfb539bb3d7fa7
Llorenç Marti Garcia <llorens.marti@gmail.com>
2016-11-03 22:37:37 -0700

    Added Worker Pass model to allow BenchmarkDefinitions to only be exeuted on some selected workers

---
+++ app/logic/benchmark/models/BenchmarkDefinitionWorkerPassModel.py
@@ -0,0 +1,33 @@
+""" BenchmarkDefinitionWorkerPass model """
+
+from django.db import models
+
+class BenchmarkDefinitionWorkerPassEntry(models.Model):
+    """ Benchmark Definition Worker Pass"""
+    definition = models.ForeignKey('benchmark.BenchmarkDefinitionEntry', related_name="worker_pass_definition")
+    worker = models.ForeignKey('bluesteelworker.WorkerEntry', related_name='worker_pass_worker')
+    allowed = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __unicode__(self):
+        return u'Benchmark Definition Worker Pass id:{0} definition:{1} worker:{2} allowed:{3}'.format(
+            self.id,
+            self.definition.id,
+            self.worker.id,
+            self.allowed,
+        )
+
+    def as_object(self):
+        """ Returns Benchmark Definition Worker Pass as an object"""
+        obj = {}
+        obj['id'] = self.id
+        obj['definition'] = {}
+        obj['definition']['id'] = self.definition.id
+        obj['definition']['uuid'] = self.definition.get_uuid()
+        obj['worker'] = {}
+        obj['worker']['id'] = self.worker.id
+        obj['definition']['uuid'] = self.worker.get_uuid()
+        obj['allowed'] = self.allowed
+
+        return obj
--- app/logic/benchmark/models/__init__.py
+++ app/logic/benchmark/models/__init__.py
@@ -1,5 +1,6 @@
 """ Automatic file """
 
 from app.logic.benchmark.models.BenchmarkDefinitionModel import BenchmarkDefinitionEntry
+from app.logic.benchmark.models.BenchmarkDefinitionWorkerPassModel import BenchmarkDefinitionWorkerPassEntry
 from app.logic.benchmark.models.BenchmarkExecutionModel import BenchmarkExecutionEntry
 from app.logic.benchmark.models.BenchmarkFluctuationOverrideModel import BenchmarkFluctuationOverrideEntry
