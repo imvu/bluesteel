0d09c7014209763d7b5c9e15d472333cf6179c73
Llorenc Marti Garcia <llorens.marti@gmail.com>
2017-06-10 12:59:26 -0700

    Now the secret key for production is obtained from a generated file. (this file will only be generated once)

---
+++ bluesteel/settings/FactorySettings.py
@@ -0,0 +1,42 @@
+import random
+import string
+import os
+import json
+
+SECRET_FILE_NAME = 'Secret-Key-File.josn'
+
+def make_random_secret_key():
+    """ Returns a secret key with length of 50 """
+
+    char_choice = "{0}{1}{2}".format(string.ascii_letters, string.digits, string.punctuation)
+    return ''.join([random.SystemRandom().choice(char_choice) for i in range(50)])
+
+def make_secret_file(file_path):
+    """ Creates a file with a json object containing the secret key. """
+
+    secret_key = make_random_secret_key()
+    with open(file_path, 'w') as secret_file:
+        secret_file.write(json.dumps({'secret_key' : secret_key}))
+
+    return secret_key
+
+def read_secret_file(file_path):
+    """ Reads the content of the secret file and returns the secret key. """
+
+    with open(file_path, 'r') as secret_file:
+        content = secret_file.read()
+        obj = json.loads(content)
+        return obj['secret_key']
+
+    print 'Secret key faile failed to open!!'
+    return ''
+
+
+def get_secret_key(secret_key_file_path):
+    """ Returns the secret key, generated if first time or read from the secret file. """
+    file_path = os.path.join(secret_key_file_path, SECRET_FILE_NAME)
+
+    if not os.path.exists(file_path):
+        return make_secret_file(file_path)
+    else:
+        return read_secret_file(file_path)
--- bluesteel/settings/production.py
+++ bluesteel/settings/production.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import sys
+import FactorySettings
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -20,7 +21,7 @@ TESTING = False
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rgs$vr920yd6i&+p2f!-xx+=+pios(bqn&j&59b%&213%7q1co'
+SECRET_KEY = FactorySettings.get_secret_key(os.path.dirname(__file__))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
