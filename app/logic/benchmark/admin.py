319137d612110f5c63f6bd8f42111f290521552e
Llorenc Marti Garcia <llorens.marti@gmail.com>
2017-08-08 22:30:19 -0700

    Added admin functions to delete BenchmarkExecutions and Feed entries.

--- app/logic/benchmark/admin.py
+++ app/logic/benchmark/admin.py
@@ -5,13 +5,26 @@ from app.logic.benchmark.models.BenchmarkDefinitionModel import BenchmarkDefinit
 from app.logic.benchmark.models.BenchmarkDefinitionWorkerPassModel import BenchmarkDefinitionWorkerPassEntry
 from app.logic.benchmark.models.BenchmarkExecutionModel import BenchmarkExecutionEntry
 from app.logic.benchmark.models.BenchmarkFluctuationOverrideModel import BenchmarkFluctuationOverrideEntry
+from app.logic.benchmark.controllers.BenchmarkExecutionController import BenchmarkExecutionController
 
 admin.site.register(BenchmarkDefinitionEntry)
 admin.site.register(BenchmarkDefinitionWorkerPassEntry)
 admin.site.register(BenchmarkFluctuationOverrideEntry)
 
+def delete_all_benchmark_executions(self, request, queryset):
+    """ Deletes all the Benchmark Executions with all its associated commands """
+    del queryset
+    entries = BenchmarkDefinitionEntry.objects.all()
+
+    for entry in entries:
+        BenchmarkExecutionController.delete_benchmark_executions(entry)
+
+    self.message_user(request, "All Benchmark executions were deleted successfully.")
+
+delete_all_benchmark_executions.short_description = "Delete All Benchmark Executions"
 
 class BenchmarkExecutionAdmin(admin.ModelAdmin):
     list_filter = ['commit__author__name', 'commit__commit_hash']
+    actions = [delete_all_benchmark_executions]
 
 admin.site.register(BenchmarkExecutionEntry, BenchmarkExecutionAdmin)
--- app/logic/gitfeeder/admin.py
+++ app/logic/gitfeeder/admin.py
@@ -2,5 +2,22 @@
 
 from django.contrib import admin
 from app.logic.gitfeeder.models.FeedModel import FeedEntry
+from app.logic.commandrepo.controllers.CommandController import CommandController
 
-admin.site.register(FeedEntry)
+def delete_all_feeds(self, request, queryset):
+    """ Delete all the feeds with all of its associated commands """
+    del queryset
+    entries = FeedEntry.objects.all()
+
+    for entry in entries:
+        CommandController.delete_command_group_by_id(entry.command_group.id)
+        entry.delete()
+
+    self.message_user(request, "All Feed entries were deleted successfully.")
+
+delete_all_feeds.short_description = "Delete All Feed Entries"
+
+class FeedAdmin(admin.ModelAdmin):
+    actions = [delete_all_feeds]
+
+admin.site.register(FeedEntry, FeedAdmin)
